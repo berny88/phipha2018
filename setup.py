54dec8429e792be98e568b59faa781a4aaf1eb19
Bernard Bougeon <bernard.bougeon@gmail.com>
2018-04-22 18:30:47 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='euroXXXVI',
+      version='1.0',
+      description='the most simple bet web site to enjoy with friends.',
+      author='Bernard Bougeon',
+      author_email='eurommxvi.foot@gmail.com',
+      url='http://www.python.org/sigs/distutils-sig/',
+      install_requires=['Flask>=0.10.1', 'pymongo>=3.2.1', 'sendgrid>=1.2']
+      )
