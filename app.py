74ed7efd1a1ad0056e4e51c5ccf3930576fee52b
Bernard Bougeon <bernard.bougeon@gmail.com>
2018-05-27 23:38:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -93,4 +93,4 @@ def ma_page_404(error):
 if __name__ == '__main__':
     print("main app.py application.run")
     print(os.environ.get('OPENSHIFT_PYTHON_PORT', 8080))
-    application.run(host='0.0.0.0', os.environ.get('OPENSHIFT_PYTHON_PORT', 8080))
+    application.run(host='0.0.0.0', port=os.environ.get('OPENSHIFT_PYTHON_PORT', 8080))
